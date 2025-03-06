e0678477994b7821946f581deec8018736e635ce
David Pérez <jdperezdu@ufpso.edu.co>
2025-03-06 15:21:17 -0500

    esta es la tarea o práctica evaluativa de llamada proyecto mini

---
+++ src/ProyectoMini.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProyectoMini
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("por favor ingrese tres números cuales quiera");
+        float Numero1 = scanner.nextFloat();
+        float Numero2 = scanner.nextFloat();
+        float Numero3 = scanner.nextFloat();
+        scanner.close();
+        float Media = 0.0f ;
+
+        Media = (Numero1 + Numero2 +Numero3) / 3;
+        System.out.println("la media de los números ingresados es: " + Media);
+    }
+}
