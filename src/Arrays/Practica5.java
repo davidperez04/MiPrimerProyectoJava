1190abb8689045157fa610c113d613ce8308f562
David Perez <jdperezdu@ufpso.edu.co>
2025-05-09 07:49:52 -0500

    nuevos ejercicios hechos

---
+++ src/Arrays/Practica5.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class Practica5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("ingrese el limite que tendra el array, es decir, hasta que tamaño puede llegar a tener el array ");
+        int limite = scanner.nextInt();
+        float[] numeros_aleatorios = new float[random.nextInt(limite);
+    }
+}
